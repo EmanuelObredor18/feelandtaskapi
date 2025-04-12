45e263aec4859adba2ed9c4774efd400d2808e72
Mike Lauren <110873019+EmanuelObredor18@users.noreply.github.com>
2025-04-12 17:46:44 -0500

    Initialize Spring Boot project with Maven wrapper and basic configuration

---
+++ src/main/java/com/mikelauwrence/feelandtaskapi/FeelandtaskapiApplication.java
@@ -0,0 +1,13 @@
+package com.mikelauwrence.feelandtaskapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeelandtaskapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeelandtaskapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mikelauwrence/feelandtaskapi/FeelandtaskapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikelauwrence.feelandtaskapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeelandtaskapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
