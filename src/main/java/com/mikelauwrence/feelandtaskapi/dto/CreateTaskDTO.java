298018c08e531e8d81eed1315758a4de169164b9
Mike Lauren <110873019+EmanuelObredor18@users.noreply.github.com>
2025-04-17 17:33:21 -0500

    refactor: TaskDTO status and priority types
    Change the data types of the status and priority fields in the
    TaskResponseDTO and CreateTaskDTO to use the Status and Priority enums.

--- src/main/java/com/mikelauwrence/feelandtaskapi/dto/CreateTaskDTO.java
+++ src/main/java/com/mikelauwrence/feelandtaskapi/dto/CreateTaskDTO.java
@@ -1,13 +1,16 @@
 package com.mikelauwrence.feelandtaskapi.dto;
 
+import com.mikelauwrence.feelandtaskapi.models.enums.Priority;
+import com.mikelauwrence.feelandtaskapi.models.enums.Status;
+
 import lombok.Builder;
 
 @Builder
 public record CreateTaskDTO(
     String title,
     String description,
-    String status,
-    String priority,
+    Status status,
+    Priority priority,
     String dueDate) {
   
 }
--- src/main/java/com/mikelauwrence/feelandtaskapi/dto/TaskResponseDTO.java
+++ src/main/java/com/mikelauwrence/feelandtaskapi/dto/TaskResponseDTO.java
@@ -1,11 +1,14 @@
 package com.mikelauwrence.feelandtaskapi.dto;
 
+import com.mikelauwrence.feelandtaskapi.models.enums.Priority;
+import com.mikelauwrence.feelandtaskapi.models.enums.Status;
+
 public record TaskResponseDTO(
     Long id,
     String title,
     String description,
-    String status,
-    String priority,
+    Status status,
+    Priority priority,
     String dueDate,
     String createdAt) {
   
